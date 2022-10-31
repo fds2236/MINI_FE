50c3be99b04aa7540b6a32b36be0e8ecf78f06fc
sb9479 <sb9479@naver.com>
2022-10-31 21:04:55 +0900

    이수빈/22.10.31

--- src/cs/Cs.js
+++ src/cs/Cs.js
@@ -1,11 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
 
+const StyledCscall = styled.div`
+    width: 30rem;
+    height: 30rem;
+    background: cornflowerblue;
+    border-radius: 50%;
+    margin: -20px;
+    position: relative;
+    display: inline-block;
+    margin: 0 auto;
+
+`;
+
+const StyledCstime = styled.div`
+    width: 30rem;
+    height: 30rem;
+    background-color: skyblue;
+    border-radius: 50%;
+    margin: -20px;
+    position: relative;
+    display: inline-block;
+    margin: 0 auto;
+`;
 
 const Cs = () => {
-
     return(
-        <>
-        </>
-    )
+        <div>
+        <StyledCscall>
+        <p>깡깡</p>
+        </StyledCscall>
+
+        <StyledCstime/>
+        </div>
+    );
 }
 
 export default Cs;
