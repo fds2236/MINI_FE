cba4a9a2b0dbf95ffc816d76718d04705eb50fab
sb9479 <sb9479@naver.com>
2022-10-31 16:13:14 +0900

    수빈/22.10.31

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import SignUp from './signup/SignUp';
 import SiteInfo from './siteinfo/SiteInfo';
 import Boards from './board/boards';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -36,7 +37,9 @@ function App() {
           <Route path='/Boards' element={<Boards />} />
       </Routes>
       </div>
+      <Footer />
     </Router>
+    
    
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+    bottom: 0;
+    margin-bottom: 0;
+    background-color: #222831;
+    position: absolute;
+    height: 20vh;
+    width: 100%;
+    padding: 10px 12px 15px 12px;
+    line-height: 25px;
+    display:flex;
+    flex-direction: column;
+`;
+
+
+const Footer = () => {
+    return (
+        <>
+            <StyledFooter>
+                <div><Link to={"/SiteInfo"} className="about">회사소개</Link></div>
+                <div><Link to={"/Agree"} className="agree">이용약관</Link></div>
+                <div><Link to={"/Privacy"} className="privacy">개인정보처리방침</Link></div>
+                <div><Link to={"/Home"} className="home">SaShoe</Link></div> <br/>
+                대표:정경수 | 소재지: 서울시 강남구 테헤란로 14길 6 남도빌딩 3층 | 사업자등록번호: 101-50-41461<br/>
+                개인정보관리책임자:이수빈 | e-mail: ‘SASHOE' '123@sashoe.co.kr' | FAX: 02-514-2236
+            </StyledFooter>
+        </>
+
+    );
+}
+
+export default Footer;
