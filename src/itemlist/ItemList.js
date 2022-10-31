cba9c956f610e816f3d5e56852e1403269c029fa
yunjeong <thkdthkd@gmail.com>
2022-10-31 12:26:40 +0900

    윤정/22.10/31

--- src/itemlist/ItemList.js
+++ src/itemlist/ItemList.js
@@ -1,9 +1,22 @@
-// 윤정 커밋 테스트
-const ItemList = () => {
+import { useEffect, useState } from "react";
+import styled from "styled-componen
+import ShoesItems from "./ShoesItem";
+
+const ShoesListBlock = styled.div`
+
+`;
+
+const ItemList = (props) => {
+    const [shoesItem, setShoes] = useState("");
+
+    useEffect(() => {
+
+    })
 
     return(
-        <>
-        </>
+        <ShoesListBlock>
+            {shoesItem.map(shoes => (<ShoesItems key={shoes.pro_code} shoes={shoes}/>))}
+        </ShoesListBlock>
     )
 }
 
