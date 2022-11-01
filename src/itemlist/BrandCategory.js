1733a4eea5a30935a38099418bed4262ec98eea1
yunjeong <thkdthkd@gmail.com>
2022-11-01 10:34:06 +0900

    윤정/22.11.01

--- src/itemlist/BrandCategory.js
+++ src/itemlist/BrandCategory.js
@@ -20,20 +20,6 @@ import { useState } from "react";
 
 // `;
 
-const Box = styled.div`
-    background: ${props => props.color || 'blue'};
-    padding: 1rem;
-    display: flex;
-    width: 1024px;
-    margin: 0 auto;
-    @media (max-width: 1024px) {
-        width: 768px;
-    }
-    @media (max-width: 768px) {
-        width: 90%;
-    }
-`;
-
 const CategoriesBlock = styled.div`
     box-sizing: border-box;
 
