f79614084c2e245518fb5d3a196d0baf2769abe5
silverbell <fds2236@naver.com>
2022-11-02 20:37:42 +0900

    은종/22/11/02

--- src/login/RePwd.js
+++ src/login/RePwd.js
@@ -11,7 +11,9 @@ const RePwd = () => {
     return(
         <div>
         <h1>비밀번호 재설정</h1>
-        {/* 아이디는 자동으로 뿌려주고 싶어 */}
+        {/* 아이디는 자동으로 뿌려주고 싶음*/}
+        {/* 비밀번호 변경 버튼 클릭시 모달창 팝업 => 비밀번호가 정상적으로 변경되었습니다 */}
+        {/* 모달의 확인버튼 클릭시 로그인 창으로 연결 */}
         <input type="password" placeholder="비밀번호"></input>
         <br/>
         <input type="password" placeholder="비밀번호 확인"></input>
