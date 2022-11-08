cb5a6b47f1fa90fbe88b5445b8d7486df558004a
doyeonS2 <1162004@naver.com>
2022-11-08 20:11:56 +0900

    도연/22.11.08

--- src/siteinfo/SiteInfo.js
+++ src/siteinfo/SiteInfo.js
@@ -20,7 +20,7 @@ const LeftTop = styled.div`
     margin-top: 30px;
     padding: 30px;
     border-radius : 5px;
-    border: solid #eeeeee;
+    border: none;
 `;
 
 
@@ -29,30 +29,27 @@ width: 400px;
     margin-left: 100px;
     padding: 30px;
     border-radius : 5px;
-    border: solid #eeeeee;
-    border-top: none;
+    border-top: solid #eeeeee 0.5px;
     .map {
             width: 335px;
-            border: solid 1px black;
+            height: 170px;
         }
 `;
 
 const RightBox = styled.div`
     width: 1000px;
     margin-right: 100px;
-    margin-top: 30px;
+    margin-bottom: 50px;
     padding: 30px;
-    border-radius : 5px;
-    border: solid #eeeeee;
-    border-left: none;
+    border-left: solid #eeeeee 0.5px;
     .memoBox {
         width: 770px;
         height: 430px;
-        margin-top: 20px;
+        margin-top: 50px;
         margin-left: 50px;
         font-style: italic;
         font-size: 40px;
-        background-color: #eeeeee;
+        border: solid rgb(0,173,181) 0.5px;
     }
     .title {
         padding-top: 30px;
@@ -66,6 +63,11 @@ const RightBox = styled.div`
     .artist {
         color: black;
     }
+    .footer {
+        margin-top: 50px;
+        color: gray;
+        font-style: italic;
+    }
 `;
 
 function SiteInfo() {
@@ -76,23 +78,24 @@ function SiteInfo() {
     <Container2>
         <div className='leftBox'>
         <LeftTop>
-            <div><p>Sa Shoe 일정</p><Calendar onChange={onChange} value={value} className='calendar'/></div>
+            <div><p><b>Sa Shoe 일정</b></p><Calendar onChange={onChange} value={value} className='calendar'/></div>
         </LeftTop>
         <LeftBottom>
-            <div><p>오시는 길</p><img className='map' alt="map" src={map} /></div>
+            <div><p><b>오시는 길</b></p><img className='map' alt="map" src={map} /></div>
         </LeftBottom>
         </div>
         
         <RightBox>
             {/* 날짜 자동으로 업데이트 됨 */}
             <div className="text-gray-500 mt-4">
-            {moment(value).format("* YYYY년 MM월 DD일 공지사항 *")} </div> 
+            <b>{moment(value).format("YYYY년 MM월 DD일 공지사항")}</b> </div> 
             
             <div className='memoBox'>
             <p className='title'><b>Sa Shoe Show Room 12월 24일 OPEN</b></p>
             <img className='img' alt="hasungwoon" src={hasungwoon} /><span>　</span><img className='img' alt="itzy" src={itzy} />
             <div className='artist'><h4><b>초대 손님: 가수 하성운 님, 가수 있지 님</b></h4><h6>하성운 - Sneakers MV / ITZY - Sneakers MV 中 (Sa Shoe 협찬 상품)</h6></div>
             </div>
+            <div className='footer'><p>주소 : 서울시 강남구 테헤란로 14길 6 남도빌딩 3층</p><p>담당자 : 김도연</p><p>전화번호 : 02)123-4567</p></div>
         </RightBox>
     </Container2>
     </>
