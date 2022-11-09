df8c8c7fc8f23c3410b784ac3477c1f899b866e8
이수빈 <sb9479@naver.com>
2022-11-09 19:23:29 +0900

    수빈/22.11.09

--- src/mypage/Mypost.js
+++ src/mypage/Mypost.js
@@ -4,13 +4,76 @@ import styled from "styled-components";
 import MiniApi from "../api/MiniApi";
 import { useState } from "react";
 
-const StylePost = styled.div`
-    .post {
-        width: 500px;
-        height: 300px;
-        border: 1px solid black;
-    }
+const Contents = styled.div`
+    width: 800px;
+    margin: 20px auto;
     
+`;
+
+const StylePost = styled.div`
+    width: 1000px;
+    margin: 50px auto;
+    display: flex;
+    flex-direction: column;
+`;
+
+const CategoryDiv = styled.div`
+    //border: 1px solid #EEEEEE;
+    width: 340px;
+    height: min-content;
+    display: flex;
+    padding: 10px;
+    border-radius: 5px;
+    //background-color: rgba(234,234,234,0.5);
+
+    justify-content: center;
+
+`;
+
+const DetailDiv = styled.div `
+    width:800px;
+
+    box-sizing: border-box;
+    border-top: 2px solid #EEEEEE;
+    border-collapse: collapse;
+
+    margin: 0 auto;
+    
+    
+    align-items: center;
+    text-align: left;
+    display: grid;
+    grid-template-columns: 2fr 5fr 2fr 1fr;
+    grid-template-rows: 50px;
+    
+    &:hover {
+        background-color: rgba(23,158,166,0.05);
+    }
+`;
+const BigContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+const Title = styled.div`
+    font-size: 20px;
+    font-weight: 700;
+`;
+const Id = styled.div`
+    font-size: 17px;
+    font-weight: 400;
+`;
+const NowCategory = styled.div`
+    font-size: 40px;
+    font-weight: 700;
+    color: rgb(23,158,166);
+
+`;
+const Content = styled.div`
+    text-align: right;
+    margin-left: 30px;
+    display: flex;
+ 
 `;
 
 // 내 게시글 보기
@@ -42,6 +105,12 @@ const Mypost = () => {
 
     }, []);
     
+    const Onclickpost= (mypost) =>{
+        window.localStorage.setMypost(whoLoginNow);
+        window.location.replace('/Board');
+    }
+    
+
 //     const onClick = async (whoLoginNow) => {
 //     try {
 //         const cmd = await MiniApi.mypostInfo(whoLoginNow, category, title, content);
@@ -59,21 +128,22 @@ const Mypost = () => {
             <hr></hr>                    
             <StylePost>
                 <br/>
-                <div>
-                    <h2 >내 게시글 보기
+                <BigContainer>
+                    <Contents >내 게시글 보기
                     {mypost && mypost.map(e => (
-                        <div className="post">
-                    
-                        <div key={e.id}>
-                            <p>{whoLoginNow}</p>
-                            <p>{e.CATEGORY}</p>
-                            <p>{e.TITLE}</p>
-                            <p>{e.CONTENT}</p>
-                        </div>
-                        </div>
+                        <DetailDiv>
+                        <DetailDiv onClick={Onclickpost} key={e.id}>
+                            <Id>{whoLoginNow}</Id>
+                            <NowCategory>{e.CATEGORY}</NowCategory>
+                            <Title>{e.TITLE}</Title>
+                            <Content>{e.CONTENT}</Content>
+                        </DetailDiv>
+                        </DetailDiv>
                     ))}
-                    </h2>
-                </div>
+                    </Contents>
+                
+                </BigContainer>
+                
             </StylePost>
         </>
     );
