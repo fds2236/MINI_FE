9ec52b5095675cdd288764f47c00fbed9a26748e
doyeonS2 <1162004@naver.com>
2022-11-09 16:33:59 +0900

    도연/22.11.09

--- src/mypage/Mypage.js
+++ src/mypage/Mypage.js
@@ -7,7 +7,6 @@ import styled from "styled-components";
 import DelModal from "../delete/DelModal";
 import { useState, useEffect  } from "react";
 
-// push push!!!!!!
 
 //------------------------------
 // 도연 - 회원탈퇴 버튼 
@@ -37,8 +36,10 @@ function Mypage() {
 
     //-------------------------------------------
     // 도연 - 회원 탈퇴 
-    const localId = window.localStorage.getItem("whoLoginNow");
-    const localPwd = window.localStorage.getItem("whoPwdNow");
+    const localId = window.localStorage.getItem("userId");
+    const localPw = window.localStorage.getItem("userPw");
+    const isLogin = window.localStorage.getItem("isLogin");
+    if(isLogin === "FALSE") window.location.replace("/");
 
     // 모달
     const [modalOpen, setModalOpen] = useState(false);
@@ -49,19 +50,12 @@ function Mypage() {
 
     const confirmModal = async() => { // 탈퇴한다고 눌렀을 때
         setModalOpen(false);
-        try{
-                //alert("아이디 패스워드 : "+ localId + localPwd);
-                const memberReg = await MiniApi.delete(localId, localPwd);
-                console.log(memberReg.data.result);
-                //alert("중간확인 : " + memberReg.data.result);
-                if(memberReg.data.result === "OK") {
-                    window.localStorage.removeItem("whoLoginNow");
-                    window.location.replace("/");
-                } else {
-                    alert("모달실행 / OK가 아님");
-                }
-        } catch(e) {
-            alert("API호출 실패 : " + e);
+        const memberReg = await MiniApi.memberDelete(localId, localPw);
+        console.log(memberReg.data.result);
+        if(memberReg.data.result === "OK") {
+            window.location.replace("/");
+        } else {
+
         }
     };
 
@@ -71,15 +65,10 @@ function Mypage() {
 
     //--------------------------------------------------------------------------
  
+
     return(
       <div>
-<<<<<<< HEAD
         <Profile/>
-=======
-        
-        <Profile/>
-        
->>>>>>> d3024db14bac39a481659416a0062c7090381675
         {/* -------------------------------------------------------------------------------
         도연 회원 탈퇴 */}
       <div onClick={onClickMemberDelete}>
