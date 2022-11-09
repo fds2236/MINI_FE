79198aeccfa364510514482a21177355b1a3d4d9
silverbell <fds2236@naver.com>
2022-11-09 18:19:42 +0900

    은종/22.11.09

--- src/itemlist/Search.js
+++ src/itemlist/Search.js
@@ -1,11 +1,66 @@
+
 import { Link } from "react-router-dom";
-import { useState} from "react";
+import { useState, useEffect} from "react";
 import MiniApi from "../api/MiniApi";
 import Modal from "../util/Modal";
 import styled from "styled-components";
 
+const ItemContainer = styled.div`
+    .item_img{
+        width : 250px;
+        height : 210px;
+        &:hover {
+
+        }
+    }
+
+
+
+`;
+
+
+const OnClick = (e) => {
+    window.localStorage.setItem("Detail", e);
+    window.location.replace("/ItemDetail");
+}
+
 
 const Search = () => {
+    const [searchInfo, setSearchInfo] = useState("");
+
+    let word = window.localStorage.getItem("word");
+
+useEffect(() => {
+    const SearchData = async () => {
+        try {
+            window.localStorage.getItem(word);
+            let response = await MiniApi.keyWord(word);
+
+            setSearchInfo(response.data);
+            console.log(response.data);
+        } catch (e) {
+            console.log(e + "검색 실패 입니다");
+        }
+    };
+    SearchData();
+}, []);
+
+return (
+    <>
+    <div>
+        {searchInfo && searchInfo.map(word => (
+        <>
+            <ItemContainer onClick={()=>OnClick(word.proCode)}>
+                <img className="item_img" src = {word.img1Path}/>
+                <div className="item_code">{word.proCode}</div>
+                <div className="item_">{word.brand}</div>
+            </ItemContainer>
+        </>
+
+        ))}
+    </div>
+    </>
+)
 
 }
 
